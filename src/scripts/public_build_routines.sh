a3ab6fe1448ac2599c90e52e951361437f476377
Yuriy Musatenko <yuriy@prismskylabs.com>
2020-10-05 15:09:42 -0700

    Extra improvements

--- src/scripts/public_build_routines.sh
+++ src/scripts/public_build_routines.sh
@@ -10,10 +10,10 @@ boostrap_install_git()
 {
     sudo apt-get update -y --force-yes
     sudo apt-get install git -y --force-yes
-    PSL_CONAN_USER=${PSL_CONAN_USER:-$USER}
+    PSL_USER=${PSL_USER:-$USER}
 
     # Cache credentials entered once for some time.
-    sudo -H -u ${PSL_CONAN_USER} git config --global credential.helper 'cache --timeout 28800'
+    sudo -H -u ${PSL_USER} git config --global credential.helper 'cache --timeout 28800'
 }
 
 boostrap_install_vim()
@@ -21,11 +21,17 @@ boostrap_install_vim()
     sudo apt-get install vim vim-scripts vim-doc -y --force-yes
     sudo update-alternatives --set editor /usr/bin/vim.basic
 
-    sudo apt install vim-youcompleteme
-    PSL_CONAN_USER=${PSL_CONAN_USER:-$USER}
+}
 
-    # https://dominoc925.blogspot.com/2019/09/installing-vim-youcompleteme-for-ubuntu.html
-    sudo -H -u ${PSL_CONAN_USER} vim-addon-manager install youcompleteme
+bootstrap_install_youcompleteme()
+{
+    # https://github.com/ycm-core/YouCompleteMe#linux-64-bit
+    PSL_USER=${PSL_USER:-$USER}
+    BUNDLE_DIR=$(sudo -H -u ${PSL_USER} bash -c 'echo "$HOME/.vim/bundle"')
+    sudo -H -u ${PSL_USER} mkdir -p $BUNDLE_DIR
+    sudo -H -u ${PSL_USER} git clone https://github.com/VundleVim/Vundle.vim.git $BUNDLE_DIR/Vundle.vim
+    sudo -H -u ${PSL_USER} vim +PluginInstall +qall
+    sudo -H -u ${PSL_USER} bash -c 'pushd  ~/.vim/bundle/YouCompleteMe;  python3 install.py --clangd-completer; popd'
 
 }
 
@@ -43,6 +49,11 @@ bootstrap_install_python()
 
 }
 
+bootstrap_install_extra_tools()
+{
+    sudo apt-get install build-essential cmake python3-dev  -y --force-yes
+}
+
 bootstrap_install_conan()
 {
     sudo python3 -m virtualenv $CONAN_ENV_DIR
@@ -54,9 +65,9 @@ bootstrap_install_conan()
 
 bootstrap_configure_conan()
 {
-    PSL_CONAN_USER=${PSL_CONAN_USER:-$USER}
-    sudo -H -u $PSL_CONAN_USER conan
-    CONAN_CONF=$(eval echo "~$PSL_CONAN_USER/.conan/conan.conf")
+    PSL_USER=${PSL_USER:-$USER}
+    sudo -H -u $PSL_USER conan
+    CONAN_CONF=$(eval echo "~$PSL_USER/.conan/conan.conf")
     # Enable revisions
    if ! grep "revisions_enabled = True           # environment CONAN_REVISIONS_ENABLED"  $CONAN_CONF; then
        CONF_TMP=$(mktemp  $CONAN_CONF.prv.XXX)
@@ -70,7 +81,9 @@ bootstrap_configure_conan()
 bootstrap_setup()
 {
     boostrap_install_git
+    bootstrap_install_extra_tools
     boostrap_install_vim
+    #bootstrap_install_youcompleteme
     bootstrap_install_python
     bootstrap_install_conan
     bootstrap_configure_conan
